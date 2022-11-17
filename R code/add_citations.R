1b36ac8be0a46c93de7ee634ef0e04b46afba78c
siyueyang <ysyyjwh@gmail.com>
2022-11-17 23:08:26 +0800

    Update the reference number system according to the latest version. This will affect prior figures in analysis report 3.

--- R code/add_citations.R
+++ R code/add_citations.R
@@ -1,14 +1,15 @@
 # Library to join based on regex.
 library("fuzzyjoin")
+library("tidyverse")
 
 # Read in citations from the paper.
-citations <- read.csv('citations.csv', header = FALSE)
+citations <- read.csv('../Data/citations.csv', header = FALSE)
 citations <- cbind(citations, 1:nrow(citations))
 colnames(citations) <- c('Title', 'Citation_Number')
 dim(citations)
 
 # Read in annotation file including selected articles.
-annotations_all <- read.csv("../Data/article_charting_101922.csv", skip = 1)
+annotations_all <- read.csv("../Data/article_charting.csv", skip = 1)
 annotations <- annotations_all %>% filter(Include_FR_SY == 1)
 dim(annotations)
 
@@ -24,9 +25,5 @@ colnames(my_citations)
 head(my_citations)
 
 # Save the file.
-write.csv(my_citations, "../Data/article_charting_101922_with_citations.csv")
+write.csv(my_citations, "../Data/article_charting_with_citations.csv")
 
-
-# test <- read.csv("../Data/article_charting_101722_with_citations.csv")
- # head(test)
-#dim(my_citations)
